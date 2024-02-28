3de751d3e6a15bf2b8b59a8eae2ef19dce701ba0
HajarIH <languages0701@gmail.com>
2024-02-28 22:21:57 +0100

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child, *previous;
-	
+
 	child = malloc(sizeof(binary_tree_t));
 	if (child == NULL || parent == NULL)
 		return (NULL);
@@ -32,3 +32,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	return (child);
 }
+
