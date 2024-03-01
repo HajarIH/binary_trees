23d13d8585f484ba7eb7a3193a909b77505ed0b0
HajarIH <languages0701@gmail.com>
2024-03-01 17:45:24 +0100

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,10 +11,10 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
+		return (1);
+	if ((!tree->left && tree->right) ||
+			(tree->left && !tree->right))
 		return (0);
-	if (xnor(tree->left, tree->right))
-		return (1 * binary_tree_is_full(tree->left) *
+	return (binary_tree_is_full(tree->left) &&
 				binary_tree_is_full(tree->right));
-	else
-		return (0);
 }
