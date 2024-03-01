2cb3c648f08585f82a1603487dffbfa6912e648d
HajarIH <languages0701@gmail.com>
2024-03-01 18:21:57 +0100

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,8 +13,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (binary_tree_is_full(tree) &&
-			binary_tree_height(tree->left) ==
-			binary_tree_height(tree->right))
+			binary_tree_balance(tree) == 0)
 		return (1);
 	else
 		return (0);
